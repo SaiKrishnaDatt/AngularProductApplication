7c97dd11c95192bc758195af05327ea7f067ed82
SaiKrishnaDatt <krishna.suddhapally@gmail.com>
2019-06-30 12:14:46 +0530

    product-list.component.ts updated

---
+++ src/app/product/product-list.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+@Component({
+    selector : 'pm-product',
+    templateUrl : './product-list.component.html'
+})
+export class ProductListComponet{
+    
+}
